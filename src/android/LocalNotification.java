4f7f1ae77bd2f525b5f1bd6e312c9f8557e39501
ochakov <evgeny@ochakov.com>
2015-06-05 17:09:48 -0400

    Update LocalNotification.java

--- src/android/LocalNotification.java
+++ src/android/LocalNotification.java
@@ -558,16 +558,7 @@ public class LocalNotification extends CordovaPlugin {
             return;
         }
 
-        webView.post(new Runnable(){
-            public void run(){
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-                    webView.evaluateJavascript(js, null);
-                } else {
-                    webView.loadUrl("javascript:" + js);
-                }
-            }
-        });
-    }
+        webView.sendJavascript(js);
 
     /**
      * Convert JSON array of integers to List.
